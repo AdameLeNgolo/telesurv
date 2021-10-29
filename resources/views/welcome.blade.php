28de0d48dcea74158f8a81a63fce3ff38014b974
adamelengolo <holndou359@gmail.com>
2021-10-29 14:31:53 +0000

    First commit with tailwindcss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    </head>
+    <body class="min-h-screen bg-grey-100 ">
+       <h1 class="text-red-900 text-3xl text-center border-4 ">Hello my big world</h1>
+    </body>
+</html>
